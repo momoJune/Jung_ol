a2b78a0d912f4d0f0e3e9d0bd837a70a12abee11
Junmo Park <jmjm9797@naver.com>
2021-11-12 21:15:11 +0900

    정올문제풀이

---
+++ JungOl_Print_12.java
@@ -0,0 +1,17 @@
+
+
+package jung_ol;
+
+public class JungOl_Print_12 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("TTTTTTTTTT");
+		System.out.println("TTTTTTTTTT");
+		System.out.println("    TT    ");
+		System.out.println("    TT    ");
+		System.out.println("    TT    ");
+	}
+
+}
---
+++ JungOl_Print_13.java
@@ -0,0 +1,17 @@
+
+
+package jung_ol;
+
+public class JungOl_Print_13 
+{
+
+	public static void main(String[] args) 
+	{
+		int krScore = 90, mathScore = 80, enScore = 100;
+		int sum = krScore + mathScore + enScore;
+		int avg = sum / 3;
+		
+		System.out.printf("kor %d\nmat %d\neng %d\nsum %d\navg %d",krScore, mathScore, enScore, sum, avg);
+	}
+
+}
